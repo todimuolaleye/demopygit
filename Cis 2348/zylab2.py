a8fa1b5bbe6189c81df008c06f881a60023e1c3f
todimuolaleye <157177737+todimuolaleye@users.noreply.github.com>
2024-02-06 20:54:00 -0600

    Add files via upload

---
+++ Cis 2348/zylab2.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+user_num1 = int(input("Enter integer:\n"))
+
+squared = user_num1 * user_num1
+cubed = user_num1 * user_num1 * user_num1
+
+print(f'{user_num1} squared is {squared}')
+print(f'And {user_num1} Cubed is {cubed} !!')
+
+user_num2 = int(input("Enter another integer:\n"))
+
+sum_of_numbers = user_num1 + user_num2
+product_of_numbers = user_num1 * user_num2
+
+print(f'{user_num1} + {user_num2} is {sum_of_numbers}')
+print(f'{user_num1} * {user_num2} is {product_of_numbers}')
+
+
